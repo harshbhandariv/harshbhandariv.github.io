68fd90fb8058883dc127d419cf31dcdb4c0fed34
Harsh Bhandari <harsh.bhandari.v@gmail.com>
2024-01-28 01:00:43 +0530

    Follow new format of setting dangerously set inner html

--- portfolio/src/components/Home/About.js
+++ portfolio/src/components/Home/About.js
@@ -3,6 +3,9 @@ import { about } from '../../assets/config';
 import photo from '../../assets/profilePhoto.jpg';
 import '../../scss/about.scss';
 export default function About() {
+  const aboutme = {
+    __html: about.aboutme
+  }
   return (
     <div className="About" id="about">
       <h1>About Me</h1>
@@ -10,7 +13,7 @@ export default function About() {
         <div className="profileDiv">
           <img src={photo} alt={`${about.firstName}`} />
         </div>
-        <div className="profileAboutDiv" dangerouslySetInnerHTML={about.aboutme} />
+        <div className="profileAboutDiv" dangerouslySetInnerHTML={aboutme} />
       </div>
     </div>
   );
